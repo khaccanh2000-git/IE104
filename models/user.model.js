287715c4a2a36520f9f4193bb1ae5bfec705585a
Khac Canh <khaccanh2000bn@gmail.com>
2021-11-20 22:27:01 +0700

    register+ header + footer

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    level:Number,
-    password: String
-})
-module.exports = mongoose.model('user', userSchema)
+  name: String,
+  old: Number,
+  place: String,
+  email: String,
+  password: String,
+});
+module.exports = mongoose.model("user", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,89 +1,102 @@
-const express=require('express')
-const userModel = require('../models/user.model')
-const bcrypt = require('bcrypt')
-const passport=require('passport')
-const router=express.Router()
+const express = require("express");
+const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const router = express.Router();
 
-router.get('/', async(req, res)=>{
-    const users=await userModel.find()
-    res.render('users/index',{users:users})
-})
-router.get('/register',(req,res)=>{
-    res.render("users/register")
-})
-function check(req,res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    res.redirect('/user/login')
+router.get("/", async (req, res) => {
+  const users = await userModel.find();
+  res.render("users/index", { users: users });
+});
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+function check(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/user/login");
 }
-router.get('/profile',check,(req,res)=>{
-
-    let name="No name"
-    if(req.user){
-        name="name: "+req.user.name
-    }
-    // console.log(value)
-    res.render('users/profile',{name:name})
-})
+router.get("/profile", check, (req, res) => {
+  let name = "No name";
+  if (req.user) {
+    name = "name: " + req.user.name;
+  }
+  // console.log(value)
+  res.render("users/profile", { name: name });
+});
 // LOGIN
-router.get('/login',(req,res)=>{
-    res.render("users/login")
-})
-router.post('/login',passport.authenticate('local',{
-    successRedirect:'/user/profile',
-    failureRedirect:'/user/login',
-    failureFlash: true
-}))
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/user/profile",
+    failureRedirect: "/user/login",
+    failureFlash: true,
+  })
+);
 
-router.post('/',async(req,res)=>{
-    try{
-        const hashedPassword=await bcrypt.hash(req.body.password,10)
-        const users=new userModel({
-            name: req.body.name,
-            email: req.body.email,
-            level:req.body.level,
-            password: hashedPassword
-        })
-        await users.save()
-        req.flash("success","Insert succesfull")
-        res.redirect('/user')
-    }
-    catch(e){
-        req.flash("error","Insert failed")
-        console.log(e)
-        res.redirect('/')
-    }
-})
+router.post("/", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const users = new userModel({
+      name: req.body.name,
+      old: req.body.old,
+      sex: req.body.sex,
+      place: req.body.place,
+      email: req.body.email,
+      level: req.body.level,
+      password: hashedPassword,
+    });
+    await users.save();
+    req.flash("success", "Insert succesfull");
+    res.redirect("/user");
+  } catch (e) {
+    req.flash("error", "Insert failed");
+    console.log(e);
+    res.redirect("/");
+  }
+});
 
-router.get('/logout',(req, res) => {
-    req.logout()
-    console.log("Logout successful")
-    res.redirect('/user/login')
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  console.log("Logout successful");
+  res.redirect("/user/login");
+});
 
-router.delete('/:id', async(req,res)=>{
-    try{
-        const user =await userModel.findByIdAndDelete(req.params.id)
-        res.redirect('/user')
-    }
-    catch(e){
-        console.log(e)
-        res.redirect('/')
-    }
-})
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await userModel.findByIdAndDelete(req.params.id);
+    res.redirect("/user");
+  } catch (e) {
+    console.log(e);
+    res.redirect("/");
+  }
+});
 
-router.get('/github',passport.authenticate('github'))
-router.get('/github/callback',passport.authenticate('github',{
-    successRedirect:'/user/profile',
-    failureRedirect:'/user/login',
-    failureFlash:true
-}))
+router.get("/github", passport.authenticate("github"));
+router.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    successRedirect: "/user/profile",
+    failureRedirect: "/user/login",
+    failureFlash: true,
+  })
+);
 
-router.get('/google',passport.authenticate('google',{scope:['profile','email']}))
-router.get('/google/callback',passport.authenticate('google',{
-    successRedirect:'/user/profile',
-    failureRedirect:'/user/login',
-    failureFlash:true
-}))
-module.exports = router
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/user/profile",
+    failureRedirect: "/user/login",
+    failureFlash: true,
+  })
+);
+module.exports = router;
